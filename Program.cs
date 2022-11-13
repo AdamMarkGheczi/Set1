8af73de31d674791293cc1ce2f0921f19afa2f10
Adam Gheczi <adamark03gmail.com>
2022-11-13 17:53:35 +0200

    Remove unnecessary usings

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 using System.Reflection;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Set_1
 {
